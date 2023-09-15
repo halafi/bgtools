1bc478f38ac57c5826d834b0355fdc3f6d9873c5
halafi <filiphalas74@gmail.com>
2023-09-15 14:44:17 +0200

    feat: init

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,190 @@
-import Image from 'next/image'
+"use client";
+
+import Image from "next/image";
+import { useSpring, animated } from "@react-spring/web";
+import { useEffect, useState } from "react";
+
+function rollSixSidedDie() {
+  return Math.floor(Math.random() * 6) + 1;
+}
 
 export default function Home() {
+  const [result, setResult] = useState("");
+  const [rollsHistory, setRollsHistory] = useState<string[]>([]);
+  const [hideHistory, setHideHistory] = useState(false);
+  // State to keep track of the animation toggle
+  const [state, toggle] = useState(true);
+  // Define the animation using the useSpring hook
+  const { x } = useSpring({
+    from: { x: 0 }, // Starting value of the animated property
+    x: state ? 1 : 0, // Ending value of the animated property based on the state
+    config: { duration: 1000 }, // Configuration for the animation, specifying the duration
+  });
+
+  useEffect(() => {
+    // Load rolls history from localStorage when the component mounts
+    const storedHistory = localStorage.getItem("rollsHistory");
+    if (storedHistory) {
+      setRollsHistory(JSON.parse(storedHistory));
+    }
+  }, []);
+
+  const rollSixSidedDice = () => {
+    toggle(false);
+    setTimeout(() => {
+      toggle(true);
+    }, 1000);
+    const randomNumber = rollSixSidedDie();
+    const result = `D6: ${randomNumber}`;
+    setResult(result);
+    updateHistory(result);
+  };
+
+  const updateHistory = (result: string | null) => {
+    if (result === null) {
+      setRollsHistory([]);
+      localStorage.setItem("rollsHistory", JSON.stringify([]));
+    } else {
+      // Update rolls history in localStorage
+      const updatedHistory = [...rollsHistory, result];
+      setRollsHistory(updatedHistory);
+      localStorage.setItem("rollsHistory", JSON.stringify(updatedHistory));
+    }
+  };
+
+  const handleRollClick = () => {
+    toggle(false);
+    setTimeout(() => {
+      toggle(true);
+    }, 1000);
+    let rolls = [];
+    let total = 0;
+
+    do {
+      const rollResult = rollSixSidedDie();
+      rolls.push(rollResult);
+      total += rollResult;
+    } while (rolls[rolls.length - 1] === 6);
+
+    const resultString = `D6+: ${rolls.join("+")} = ${total}`;
+    setResult(resultString);
+    updateHistory(resultString);
+  };
+
+  function rollDnDPercentage() {
+    toggle(false);
+    setTimeout(() => {
+      toggle(true);
+    }, 1000);
+    const die1 = Math.floor(Math.random() * 10);
+    const die2 = Math.floor(Math.random() * 10);
+    const percentage = die1 * 10 + die2;
+    const result = percentage === 0 ? 100 : percentage;
+    setResult(`D%: ${result}%`);
+    updateHistory(`D%: ${result}%`);
+  }
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+    <main className="flex min-h-screen flex-col items-center">
+      <div className="flex flex-col items-center mt-16">
+        <div className="font-bold text-5xl h-16 transition ease-in-out delay-150 my-8 box-content">
+          <animated.div
+            style={{
+              opacity: x.to({ range: [0, 1], output: [0.3, 1] }),
+              transform: x
+                .to({
+                  range: [0, 0.25, 0.35, 0.45, 0.55, 0.65, 0.75, 1],
+                  output: [1, 0.97, 0.9, 1.1, 0.9, 1.1, 1.03, 1],
+                })
+                .to((x) => `scale(${x})`),
+            }}
           >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+            {result.split(":")[1]}
+          </animated.div>
+        </div>
+
+        {rollsHistory.length > 0 && !hideHistory && (
+          <div className="w-72 my-16">
+            <h2>Past throws</h2>
+            <ul className="max-h-32 overflow-y-auto text-sm text-gray-500">
+              {rollsHistory.reverse().map((roll, index) => (
+                <li key={index}>{roll}</li>
+              ))}
+            </ul>
+            <div className="flex space-x-2 mt-2">
+              <div
+                className="text-white font-bold cursor-pointer"
+                onClick={() => updateHistory(null)}
+              >
+                clear
+              </div>
+              <div
+                className="text-white font-bold cursor-pointer"
+                onClick={() => setHideHistory(true)}
+              >
+                hide
+              </div>
+            </div>
+          </div>
+        )}
+        <div className="mb-16 grid text-center">
+          <div
+            onClick={rollDnDPercentage}
+            className="select-none cursor-pointer group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          >
+            <h2 className="flex mb-3 text-2xl font-semibold">
+              D%{" "}
+              <div className="ml-2 flex transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+                <Image
+                  src="/10_sided_die.svg"
+                  alt="10 sided die"
+                  width={28}
+                  height={28}
+                />
+                <Image
+                  src="/10_sided_die.svg"
+                  alt="10 sided die"
+                  width={28}
+                  height={28}
+                />
+              </div>
+            </h2>
+            <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
+              Percentage dice roll.
+            </p>
+          </div>
+
+          <div
+            onClick={rollSixSidedDice}
+            className="select-none cursor-pointer group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          >
+            <h2 className={`flex mb-3 text-2xl font-semibold`}>
+              D6{" "}
+              <div className="ml-2 flex transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+                🎲
+              </div>
+            </h2>
+            <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
+              Single D6 roll.
+            </p>
+          </div>
+
+          <div
+            onClick={handleRollClick}
+            className="select-none cursor-pointer group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          >
+            <h2 className={`flex mb-3 text-2xl font-semibold`}>
+              D6+{" "}
+              <div className="ml-2 flex transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+                🎲❓🎲
+              </div>
+            </h2>
+            <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
+              D6+ roll, subsequent throw of 6 adds up.
+            </p>
+          </div>
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
     </main>
-  )
+  );
 }
